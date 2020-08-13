c32dfd6c1d0433c94aaa767ab48ae401812c468e
Michael Burgess <mkburges@gmail.com>
2020-08-13 15:27:54 -0700

    Added Weather route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET weather listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('---- indexRouter');
+	// Comment out this line:
+  //res.send('respond with a resource');
+
+  // And insert something like this instead:
+  res.json({
+  	mean: 1,
+    median: 4,
+    mode: 4
+  });
 });
 
 module.exports = router;
--- routes/weather.js
+++ routes/weather.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET weather listing. */
 router.get('/', function(req, res, next) {
+  console.log('---- weatherRouter');
 	// Comment out this line:
   //res.send('respond with a resource');
 
   // And insert something like this instead:
-  res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
-  }]);
+  res.json({
+  	mean: 1,
+    median: 4,
+    mode: 4
+  });
 });
 
 module.exports = router;
