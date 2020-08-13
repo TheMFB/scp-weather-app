1ee107b3e97527a833666f0163b9e4dff619521a
Michael Burgess <mkburges@gmail.com>
2020-08-13 16:23:23 -0700

    Moved to index, using cors

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var weatherRouter = require('./routes/weather');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/weather', weatherRouter);
+app.use('/*', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
