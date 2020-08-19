25e71997d009d6d04e5345f4dff54dab02569bcd
Michael Burgess <mkburges@gmail.com>
2020-08-19 06:18:44 -0700

    added file

---
+++ helpers/mock_data.js
@@ -0,0 +1,16 @@
+exports.averages =         {
+    low: {
+    mean: 60,
+    median: 60,
+    mode: [59, 60, 61]
+},
+mid: {
+    mean: 61,
+    median: 61,
+    mode: [60, 61, 62]
+},
+high: {
+    mean: 62,
+    median: 62,
+    mode: [61, 62, 63]
+}}
